0eff0f1bb82ebe072d68f61ae58c5918f04e6b0a
Akshay Kumar <huzaifahmahida@gmail.com>
2021-10-29 14:18:31 +0500

    update some files

--- config/app.js
+++ config/app.js
@@ -8,9 +8,8 @@ var app = express();
 // view engine setup
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static("public"));
 app.set("views", "views");
 app.set('view engine', 'ejs');
 app.use("/",router);
-app.use(express.static('public'))
+app.use(express.static('public'));
 module.exports = app;
